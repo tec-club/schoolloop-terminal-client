a46f99783cef4e952206b1f534b3078f13fc1a10
Alex Wendland <me@alexwendland.com>
2014-05-09 20:03:56 -0700

    First commit

---
+++ Regex.java
@@ -0,0 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Write a description of class Regex here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Regex
+{
+    
+    private Pattern pattern;
+    
+    public Regex(String p)
+    {
+        pattern = Pattern.compile(p);
+    }
+    
+    public String firstGroup(String string) {
+        Matcher m = pattern.matcher(string);
+        if (m.find()) {
+            return m.group(1);
+        } else
+            return null;
+    }
+}
